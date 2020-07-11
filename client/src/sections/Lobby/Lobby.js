2bf21b92d9f773dbc03251ccaaa049b202b06a7c
Ray Ye <yer@purdue.edu>
2020-07-11 03:20:16 -0400

    Revert "New Room Update"
    This reverts commit e6983b441d068f34f593e9166bb163d8e69aa5b7.

--- client/src/sections/Lobby/Lobby.js
+++ client/src/sections/Lobby/Lobby.js
@@ -8,11 +8,8 @@ class Lobby extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            show: false,
-            room: []
+            show: false
         }
-
-        this.addRoom = this.addRoom.bind(this);
     }
 
     showModal = () => {
@@ -23,44 +20,18 @@ class Lobby extends React.Component {
         this.setState({ show: false });
     }
 
-    addRoom(topic, host, numDebaters) {
-        this.setState(prevState => ({
-            room : [...prevState.room, { topic, host, numDebaters }]
-        }));
-    }
-
     render () {
         return (
             <div className="outsideContainer">
                 <h1>Argue.Me</h1>
-                <div className="topSection">
+                <div clasName="topSection">
                     <button onClick={this.showModal}>New Room</button>
                     <select className="role-selection">
                         <option>Debater</option>
                         <option>Judge</option>
                         <option>Spectator</option>
                     </select>
-                    <RoomModal show={this.state.show} handleClose={this.hideModal} addRoom={this.addRoom} />
-                    <table className="myTable">
-                        <thead>
-                            <th>Topic</th>
-                            <th>Host</th>
-                            <th># of Debaters</th>
-                            <th>Join Room</th>
-                        </thead>
-                        <tbody>
-                            {this.state.room.map((room) => {
-                                return (
-                                    <tr>
-                                        <td>{room.topic}</td>
-                                        <td>{room.host}</td>
-                                        <td>{room.numDebaters}</td>
-                                        <button>Join Room</button>
-                                    </tr>
-                                );
-                            })}
-                        </tbody>
-                    </table>
+                    <RoomModal show={this.state.show} handleClose={this.hideModal} />
                 </div>
             </div>
         )
--- client/src/sections/Lobby/roomModal/RoomModal.js
+++ client/src/sections/Lobby/roomModal/RoomModal.js
@@ -2,56 +2,36 @@ import React from 'react';
 
 import './RoomModal.css'
 
-class RoomModal extends React.Component{
-    constructor(props) {
-      super(props)
-      this.formSubmit = this.formSubmit.bind(this);
-    }
+const roomModal = ({ handleClose, show }) => {
+    const showHideClassName = show ? "modal display-block" : "modal display-none";
+  
+    return (
+      <div className={showHideClassName}>
+        <section className="modal-main">
+            <span class="close" onClick={handleClose}>&times;</span>
+            <h2>Create Room</h2>
+            <input type="text" placeholder="User Name" name="title" className="inputBox" required />
+            <input type="text" placeholder="Topic" name="title" className="inputBox" required />
+            <select className="num-people">
+                <option>Number of People: 2</option>
+                <option>Number of People: 4</option>
+                <option>Number of People: 6</option>
+                <option>Number of People: 8</option>
+            </select> <br/>
+            <div className="radio-buttons">
+                <label>
+                    <input type="radio" value="option1" name="side" />
+                        For
+                </label>
+                <label>
+                        <input type="radio" value="option2" name="side" />
+                        Against
+                </label>
+            </div>
+            <button style={{margin: "5%"}} onClick={handleClose}>Create</button>
+        </section>
+      </div>
+    )
+  }
 
-    formSubmit(event) {
-      event.preventDefault()
-      const form = event.target
-      const topic = form.elements["topic"].value
-      const host = form.elements["userName"].value
-      const numDebaters = form.elements["num"].value
-      this.props.addRoom(topic, host, numDebaters)
-      form.reset()
-    }
-
-    render () {
-      const showHideClassName = this.props.show ? "modal display-block" : "modal display-none"
-      return (
-        <div className={showHideClassName}>
-          <section className="modal-main">
-              <span class="close" onClick={this.props.handleClose}>&times;</span>
-              <form onSubmit={this.formSubmit}>
-                <h2>Create Room</h2>
-                <input type="text" placeholder="User Name" name="title" className="inputBox" id="topic" required />
-                <input type="text" placeholder="Topic" name="title" className="inputBox" id="userName" required />
-                <select className="num-people" id="num">
-                    <option disabled selected>Number of People</option>
-                    <option selected>2</option>
-                    <option>4</option>
-                    <option>6</option>
-                    <option>8</option>
-                </select> <br/>
-                <div className="radio-buttons" required>
-                    <label>
-                        <input type="radio" value="option1" name="side" />
-                            For
-                    </label>
-                    <label>
-                            <input type="radio" value="option2" name="side" />
-                            Against
-                    </label>
-                </div>
-                <input style={{margin: "5%"}}  type="submit" value="Create" />
-              </form>
-          </section>
-        </div>
-      )
-    }
-    
-} 
-
-export default RoomModal 
+export default roomModal 
