e6983b441d068f34f593e9166bb163d8e69aa5b7
Ray Ye <yer@purdue.edu>
2020-07-11 02:59:52 -0400

    New Room Update

--- client/src/sections/Lobby/Lobby.js
+++ client/src/sections/Lobby/Lobby.js
@@ -8,8 +8,11 @@ class Lobby extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            show: false
+            show: false,
+            room: []
         }
+
+        this.addRoom = this.addRoom.bind(this);
     }
 
     showModal = () => {
@@ -20,18 +23,44 @@ class Lobby extends React.Component {
         this.setState({ show: false });
     }
 
+    addRoom(topic, host, numDebaters) {
+        this.setState(prevState => ({
+            room : [...prevState.room, { topic, host, numDebaters }]
+        }));
+    }
+
     render () {
         return (
             <div className="outsideContainer">
                 <h1>Argue.Me</h1>
-                <div clasName="topSection">
+                <div className="topSection">
                     <button onClick={this.showModal}>New Room</button>
                     <select className="role-selection">
                         <option>Debater</option>
                         <option>Judge</option>
                         <option>Spectator</option>
                     </select>
-                    <RoomModal show={this.state.show} handleClose={this.hideModal} />
+                    <RoomModal show={this.state.show} handleClose={this.hideModal} addRoom={this.addRoom} />
+                    <table className="myTable">
+                        <thead>
+                            <th>Topic</th>
+                            <th>Host</th>
+                            <th># of Debaters</th>
+                            <th>Join Room</th>
+                        </thead>
+                        <tbody>
+                            {this.state.room.map((room) => {
+                                return (
+                                    <tr>
+                                        <td>{room.topic}</td>
+                                        <td>{room.host}</td>
+                                        <td>{room.numDebaters}</td>
+                                        <button>Join Room</button>
+                                    </tr>
+                                );
+                            })}
+                        </tbody>
+                    </table>
                 </div>
             </div>
         )
