88ffe8fb6ef349c9ac02e0f667e5ab1dd3f9f1e8
Ray Ye <yer@purdue.edu>
2020-07-11 03:22:22 -0400

    Test

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,11 @@ import React from 'react'
 
 import { BrowserRouter as Router, Route} from 'react-router-dom'
 
-import Lobby from './sections/Lobby/Lobby'
-import Chat from './sections/Chat'
-
 const App = () => {
     return (
         <Router>
             <Route path="/" exact component={Lobby} />
-            <Route path="/Chat" component={Chat} /> {/* Depending on what Chat is called */}
+            <Route path="/chat" component={Chat} />
         </Router>
     )
 }
-
-export default App;
